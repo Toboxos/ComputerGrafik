451dd53bfa4a33276683a5ee6f8cc1cb8092c8d4
Airtoni <blacktoni1399@gmail.com>
2020-12-29 20:07:22 +0100

    Explosion Model and explosion force scripts added

---
+++ Assets/Scripts/ExchangetoExplosionModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExchangetoExplosionModel : MonoBehaviour
+{
+    public GameObject firstModel;
+    public GameObject explosionModel;
+  
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            SpawnExplosionModel();
+        }
+    }
+    public void SpawnExplosionModel()
+    {
+        Destroy(firstModel);
+        GameObject explosionObject = Instantiate(explosionModel) as GameObject;
+        explosionObject.GetComponent<ExplodeScript>().Explode();
+    }
+}
---
+++ Assets/Scripts/ExplodeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeScript : MonoBehaviour
+{
+
+    public float force;
+    public float radius;
+
+    void Start()
+    {
+        Explode();
+    }
+
+    public void Explode()
+    {
+        foreach(Transform t in transform)
+        {
+            var rb = t.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+           
+        }
+    }
+}
